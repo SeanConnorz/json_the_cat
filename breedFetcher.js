5b9504f673835bfbc3a914dedf3f679a2f2a4f2d
Connor Hughes <seanandhughes@gmail.com>
2021-11-04 20:49:33 +0000

    created a test file that assures that the program works

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,26 @@
 const request = require('request');
-const myArgs = process.argv.slice(2);
 
-const dynamicBreedName = (breed) => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
     if (error) {
-      console.log('Something went wrong:', error);
+      callback(error, null);
       return;
     }
     
     const data = JSON.parse(body);
 
     if (data.length === 0) {
-      console.log('Breed not found.');
+      callback('Breed not found', null);
       return;
     }
 
-    console.log(data[0].description);
+    callback(null, data[0].description);
   });
-}
 
-dynamicBreedName(myArgs[0]);
+
+};
+
+module.exports = {
+  fetchBreedDescription,
+};
